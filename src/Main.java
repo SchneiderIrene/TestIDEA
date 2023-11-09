f2701a2e176cbb3ccd0d02cd27944d7384a27dbd
Schneider Irene <schneider_irene@outlook.com>
2023-11-09 19:39:22 +0100

    HA 09/11/23

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        double a = 25;
+        double b = 3;
+        double res = a/3;
+        System.out.println(res);
+        System.out.println(a%b);
+
+        int c = 10;
+        int sq = c*c;
+        System.out.println(sq);
+    }
+}
