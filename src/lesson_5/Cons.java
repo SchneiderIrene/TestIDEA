70a9205453cae5c405ff5a981e20754f9be23a93
Schneider Irene <schneider_irene@outlook.com>
2023-11-13 17:02:07 +0100

    cons weekday and vocation

---
+++ src/lesson_5/Cons.java
@@ -0,0 +1,26 @@
+package lesson_5;
+
+public class Cons {
+    public static void main(String[] args) {
+        /*
+        weekday = день недели
+        vacation = отпуск
+        false false = true
+        false true = true
+        true false = false
+
+         */
+
+        boolean weekDay;
+        boolean vocation;
+
+        weekDay = true;
+        vocation = false;
+
+            System.out.println(!weekDay||vocation);
+            System.out.println(weekDay==false||vocation);
+            System.out.println(weekDay==false&&vocation);
+
+
+    }
+}
