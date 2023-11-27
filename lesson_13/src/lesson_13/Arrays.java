1b259af58406eb38aa92b2f389961cc777daf4d9
Schneider Irene <schneider_irene@outlook.com>
2023-11-27 11:04:21 +0100

    lesson 13

---
+++ lesson_13/src/lesson_13/Arrays.java
@@ -0,0 +1,21 @@
+package lesson_13;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] sample = new int[10];
+        sample[0] = 45;
+        sample[1] = 30;
+        sample[2] = 34;
+        sample[3] = 307;
+        System.out.println("Zero elements of sample array is " + sample[0]);
+        int [] numbers = new int[]{1,23,3,4,5,6,7};
+        int [] arr = {1,2,3,4,5,6,7,8,9,10};
+        System.out.print(arr[0] + " " +  arr[1]);
+        System.out.println();
+        System.out.println("---------------------------------------");
+        System.out.println(arr.length);
+        int len = arr.length;
+        System.out.println(len);
+    }
+    public static void abc (){}
+}
