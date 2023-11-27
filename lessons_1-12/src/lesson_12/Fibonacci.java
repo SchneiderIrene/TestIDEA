0777ab8c3de7a30880ccc0f6f8d07dbe7ac5cb0a
Schneider Irene <schneider_irene@outlook.com>
2023-11-27 18:47:30 +0100

    lesson 13 Fibonacci

--- lessons_1-12/src/lesson_12/Fibonacci.java
+++ lessons_1-12/src/lesson_12/Fibonacci.java
@@ -2,17 +2,50 @@ package lesson_12;
 
 public class Fibonacci {
     public static void main(String[] args) {
+
+        System.out.println(fibonacci(0));
+        System.out.println("-----------------");
+        System.out.println(fibonacci(1));
+        System.out.println("-----------------");
+        System.out.println(fibonacci(2));
+        System.out.println("-----------------");
+        System.out.println(fibonacci(3));
+        System.out.println("-----------------");
+        System.out.println(fibonacci(4));
+        System.out.println("-----------------");
+        System.out.println(fibonacci(5));
+        System.out.println("-----------------");
+        System.out.println(fibonacci(6));
+        System.out.println("-----------------");
         System.out.println(fibonacci(7));
+        System.out.println("-----------------");
+        System.out.println(fibonacci(8));
+        System.out.println("-----------------");
+        System.out.println(fibonacci(9));
+        System.out.println("-----------------");
     }
-    static int fibonacci (int n){
-        int res = 0;
+    public static int fibonacci (int n){
+//        if (n==0) {
+//            System.out.println(n);
+//            return n;
+//        }
+//        else if (n==1) {
+//            System.out.print(0 );
+//            System.out.print(1);
+//            return n;
+//        }
+
+        if (n<2){
+            return n;
+        }
         int a = 0;
         int b =1;
-        for (int i = 0; i<n; i++){
+        int res = 0;
+
+        for (int i = 2; i<=n; i++){
             res = a+b;
             a = b;
             b = res;
-            System.out.print(res + " ");
         }
         System.out.println();
         return res;
