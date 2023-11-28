00b69162d7f599dc6e48605e0848d42b562c7b2b
Schneider Irene <schneider_irene@outlook.com>
2023-11-28 15:54:35 +0100

    compare arrays

---
+++ lesson_14/src/lesson_14/ArrArr.java
@@ -0,0 +1,22 @@
+package lesson_14;
+
+public class ArrArr {
+    public static void main(String[] args) {
+        int arr1[] = {1,2,3};
+        int arr2[] = {1,2,7};
+        System.out.println(equalArrays(arr1, arr2) ? " равны " : "не равны");
+
+    }
+    public static boolean equalArrays (int arr1[], int arr2[]){
+        if (arr1.length!=arr2.length) {
+            return false;
+        }
+            for (int i = 0; i<arr1.length; i++){
+                if (arr1[i]!=arr2[i]) {
+                    return false;
+                }
+            }
+        return true;
+        }
+    }
+
