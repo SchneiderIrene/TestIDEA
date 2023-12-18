431a03ed3a3a0c1dc907c989c08caa2b9d1a035c
Schneider Irene <schneider_irene@outlook.com>
2023-12-18 17:02:45 +0100

    ArrayFibonacci

--- lesson_25/src/cons/ArrayFibonacci.java
+++ lesson_25/src/cons/ArrayFibonacci.java
@@ -45,7 +45,7 @@ import java.util.Arrays;
         }
 
         public static int fibonacci (int n){
-            int [] fibonacci = new int[n];
+            int [] fibonacci = new int[n+1];
             if (n<2){
                 return n;
             }
@@ -53,10 +53,10 @@ import java.util.Arrays;
             fibonacci[0] = 0;
             fibonacci[1] = 1;
 
-            for (int i = 2; i<n; i++){
+            for (int i = 2; i<fibonacci.length; i++){
                 fibonacci [i] = fibonacci[i-1] + fibonacci[i-2];
             }
-            return fibonacci[n-1];
+            return fibonacci[n];
         }
     }
 
